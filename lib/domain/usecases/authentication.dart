c07bc56c728dcaff439cc36529ee09ddd19c8a39
Aline Ayumi <aline.ayumi@unico.io>
2022-05-08 00:30:30 -0300

    refactor: changed auth usecase to receive a model

--- lib/domain/usecases/authentication.dart
+++ lib/domain/usecases/authentication.dart
@@ -1,5 +1,15 @@
 import 'package:clean_flutter_app/domain/entities/entities.dart';
 
 abstract class Authentication {
-  Future<AccountEntity> auth({required String email, required String password});
+  Future<AccountEntity> auth(AuthenticationParams params);
+}
+
+class AuthenticationParams {
+  final String email;
+  final String password;
+
+  AuthenticationParams({
+    required this.email,
+    required this.password,
+  });
 }
